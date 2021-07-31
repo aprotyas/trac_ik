86762dc876ec2552dcff65d4de9dba01fa7d404b
Abrar Rahman Protyasha <abrar@openrobotics.org>
2021-07-31 03:30:22 -0400

    Correctly use of `std::numeric_limits<T>::epsilon`
    Previously, `T` == `double`, which did not match expected behavior from
    the original implementation, where `T` == `float`.
    
    Signed-off-by: Abrar Rahman Protyasha <abrar@openrobotics.org>

--- trac_ik_lib/src/kdl_tl.cpp
+++ trac_ik_lib/src/kdl_tl.cpp
@@ -159,7 +159,7 @@ int ChainIkSolverPos_TL::CartToJnt(const KDL::JntArray &q_init, const KDL::Frame
 
     Subtract(q_out, q_curr, q_out);
 
-    if (q_out.data.isZero(std::numeric_limits<double>::epsilon()))
+    if (q_out.data.isZero(std::numeric_limits<float>::epsilon()))
     {
       if (rr)
       {
